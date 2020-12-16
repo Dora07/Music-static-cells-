d0bcb56a6f1b23aa766ac2ea53be814b1c104409
Dora <domonshieh@gmail.com>
2020-12-16 18:33:30 +0800

    one

--- Music(static cells)/ViewController.swift
+++ Music(static cells)/ViewController.swift
@@ -1,17 +1,29 @@
-//
-//  ViewController.swift
-//  Music(static cells)
-//
-//  Created by 謝孟芩 on 2020/12/16.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    @IBOutlet weak var Properties: UIImageView!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //設定漸層背景為圓角
+        Properties.clipsToBounds = true
+       Properties.layer.cornerRadius = 30
+        
+        //設定個人照片像IG一樣有圓環
+        //設定圖片變為圓角
+        imageView.layer.cornerRadius = (imageView.frame.size.width ) / 2
+        imageView.clipsToBounds = true
+        //設定圓環線的寬度
+        imageView.layer.borderWidth = 1.0
+        //設定圓環線的顏色
+        imageView.layer.borderColor = UIColor.white.cgColor
+        
     }
 
 
